0ad6ab8df21173b7b5166692494da3241cf34611
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2026-06-29 19:56:37 +0300

    Add home_cards.js

---
+++ My_Blog/src/data/home_cards.js
@@ -0,0 +1,22 @@
+const HOME_CARDS = [
+  {
+    title: "Blog",
+    description: "I publish at least one article or writeup weekly.",
+    url: "/blog",
+    image: "blog.png",
+  },
+  {
+    title: "Series",
+    description: "Browse articles grouped by series.",
+    url: "/series",
+    image: "series.png",
+  },
+  {
+    title: "Tags",
+    description: "Quick way to find posts by topic or keyword.",
+    url: "/blog/tags",
+    image: "tags.png",
+  },
+];
+
+export default HOME_CARDS;
