4ab725864dafb0d161ca03e86115155520282d32
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2026-06-30 16:23:27 +0300

    Add data for series.js.

---
+++ My_Blog/src/data/series.js
@@ -0,0 +1,14 @@
+const SERIES_DATA = [
+  {
+    name: "Exploring Ghidra",
+    description: "Learn, understand, and dive deep into the realm of Ghidra.",
+    image: "/img/series/ghidra.png",
+  },
+  {
+    name: "End your VI suffering",
+    description: "Explore and dip your toes into the world of one of the most versatile text editors out there: VI",
+    image: "/img/series/vi.png",
+  },
+];
+
+export default SERIES_DATA;
