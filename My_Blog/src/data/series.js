8a60234dcf8188a78eefb5ac3ab2bc68eab57470
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2026-09-21 00:07:25 +0300

    Add new entry.

--- My_Blog/src/data/series.js
+++ My_Blog/src/data/series.js
@@ -14,6 +14,11 @@ const SERIES_DATA = [
     description: "Write-ups for CTF challenges I solved during the H7CTF International 2024 competition.",
     image: "/img/series/h7ctf-international-2024.png",
   },
+  {
+    name: "The 100 Reverse Engineering Challenges Challenge",
+    description: "Write-ups, walkthroughs, and reflections from my journey through 100 reverse engineering challenges to sharpen my skills.",
+    image: "/img/series/the_100_re_challenges_challenge.png",
+  },
 ];
 
 export default SERIES_DATA;
